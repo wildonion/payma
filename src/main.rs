16428238c98f5f59aa18e895e48587e07bc9b758
wildonion <marefimoghaddam@unixerr.com>
2023-02-09 19:26:25 +0100

    add codes

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+use ring::*;
+
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+    
+    // https://github.com/skerkour/black-hat-rust/tree/main/ch_11
+
+    // TODO 
+    // encrypt the whole os using ring based on keypairs
+    // we can decrypt it with private key only and the victim
+    // must pay for the private key
+    // ...
+
+    // TODO
+    // software update using digital signature
+    // ...
+
+
+
+    Ok(())
+
+}
