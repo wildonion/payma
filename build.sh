a5a6a40941244ffb64b69e60d1acd2a187becb3a
dewo <abarmardeatashyne@gmail.com>
2023-11-29 10:53:58 +0330

    add installation scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+set -e
+sudo npm i wasm-opt -g
+curl https://get.wasmer.io -sSfL | sh
+curl https://wasmtime.dev/install.sh -sSf | bash
 rustup target add wasm32-wasi # compilation target for wasm32 wasi WebAssembly
 # rustup target add wasm32-unknown-unknown # compilation target for browser-based WebAssembly
 cargo build --bin payma --target wasm32-wasi --release 
